f6e2cda195460658a6aea6bd72a38c4c270d3684
hidin <hidingw@gmail.com>
2017-07-29 16:58:13 +0300

    29-07-2017
    Change API
    Done widget for current time.
    TODO: Add wildcards for weathe for 5 days. On wildcards post 8 string
    and 4 column(time, temp, weather, wind)

--- script/script.js
+++ script/script.js
@@ -7,22 +7,40 @@
 
 var weatherData;
 var currentTempUnit = "c";
+var currentTime;
 
+function getDate(dateUNIX){
+  var a = new Date(dateUNIX * 1000);
+  var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+  var year = a.getFullYear();
+  var month = months[a.getMonth()];
+  var date = a.getDate();
+  var hour = a.getHours();
+  // var min = a.getMinutes();
+  // var time = hour + ':' + min ;
+  var fullDate = date + " " + month + " " + year;
+  $(".date").append("<p>" + fullDate + "</p>");
+  currentTime = hour;
+}
+function setBackground(hours) {
+  if(parseInt(hours) > 20){
+    $( document.body ).css( "background-image",'url("http://oopstyle.net/wp-content/uploads/2017/06/City-Background-Night-Street-RO.jpg")' );
+  } else if(hours <= 20){
+    $( document.body ).css( "background-image", 'url("http://zellox.com/wp-content/uploads/2016/05/website-backgrounds-%E2%80%AB1%E2%80%AC-%E2%80%AB%E2%80%AC.jpg")' );
+  }
+}
 function getFahrenheitTemp(data) {
-  $(".info--currentT").prepend((data.main.temp * 1.8) + 32);
-  $(".info--maxT").prepend((data.main.temp_max * 1.8) + 32);
-  $(".info--minT").prepend((data.main.temp_min * 1.8) + 32);
+  $(".info--currentT").prepend("<span>" + ((data.main.temp * 1.8) + 32) + "</span>");
 }
 function getCelsiusTemp(data) {
-  $(".info--currentT").prepend(data.main.temp);
-  $(".info--maxT").prepend(data.main.temp_max);
-  $(".info--minT").prepend(data.main.temp_min);
+  $(".info--currentT").prepend("<span>" + (data.main.temp) + "</span>");
 }
-function showPosition(position){
+function getWeather(position){
   $.ajax({
-      url:  "https://fcc-weather-api.glitch.me/api/current?lat="
+      url:  "http://api.openweathermap.org/data/2.5/weather?lat="
         + position.coords.latitude
-        + "&lon=" + position.coords.longitude,
+        + "&lon=" + position.coords.longitude
+        + "&APPID=f3802e3a51821907ffe2a930031d46f0&units=metric",
       method: "GET",
       dataType: "json",
       jsonp: false,
@@ -35,7 +53,8 @@ function showPosition(position){
 }
 function showWeather(data){
   $( ".city" ).append(data.name);
-  $( "#currentWeatherIcon" ).addClass("wi-owm-" + data.weather[0].id);
+  $(".current-weather").prepend("<span> Now: " + data.weather[0].description + "</span>")
+  $( ".current-weather > i" ).addClass("wi-owm-" + data.weather[0].id);
   if(currentTempUnit === "c") {
     getCelsiusTemp(data);
     $(".info > p > i:not(:last)").addClass("wi-celsius");
@@ -63,31 +82,34 @@ function showError(error) {
 }
 function getLocation() {
     if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(showPosition, showError);
+        navigator.geolocation.getCurrentPosition(getWeather, showError);
     } else {
-        $(".city").append("Geolocation is not supported by this browser.");
-        $(".city").css("color", "Red");
+        $(".city").append("Geolocation is not supported by this browser.").css("color", "Red");
     }
 }
 
 $(document).ready(function() {
   getLocation();
+  $(document).ajaxComplete(function(){
+    getDate(weatherData.dt);
+    showWeather(weatherData);
+    setBackground(currentTime);
+  });
   $(".info > p > i:not(:last)").on("click", function(){
     if(currentTempUnit === "c"){
       currentTempUnit = "f";
-      $(".info > p > i:not(:last)").removeClass("wi-celsius");
-      $(".info > p > i:not(:last)").addClass("wi-fahrenheit");
+      $(".info > p > i:not(:last)").removeClass("wi-celsius").addClass("wi-fahrenheit");
+      $(".info > p:not(:last) > span").empty();
+      $(".info > p:not(:last) > span").textContent = getFahrenheitTemp(weatherData);
     } else if (currentTempUnit === "f"){
       currentTempUnit = "c";
-      $(".info > p > i:not(:last)").removeClass("wi-fahrenheit");
-      $(".info > p > i:not(:last)").addClass("wi-celsius");
-      //TODO: update data about temp
+      $(".info > p > i:not(:last)").removeClass("wi-fahrenheit").addClass("wi-celsius");
+      $(".info > p:not(:last) > span").empty();
+      $(".info > p:not(:last) > span").textContent = getCelsiusTemp(weatherData);
     }
   });
-  });
-$(document).ajaxComplete(function(){
-  showWeather(weatherData);
 });
+
 
 
 
